257e367105c0c0b889f6100462ca16cc753da55b
Ferdinand G Rios <frios@viciouscircle.com>
2025-06-21 13:26:24 -0700

    Initial Commit

---
+++ Best Seller List/Best_Seller_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Best_Seller_ListApp.swift
+//  Best Seller List
+//
+//  Created by Feldy Rios on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Best_Seller_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
